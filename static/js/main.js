510b5cbd4873baa75b9d610a71964747473fb044
Hr0mE <95316942+Hr0mE@users.noreply.github.com>
2023-10-30 16:40:21 +0300

    added owl carousel
    mb smth more

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	/*
+	var counter = 0;
+	$("body").keydown(function(event){
+		if(event.which === 37 || event.which === 65){
+			counter = (counter-1)%5;
+			if (counter < 0) {counter += 5;}
+		}
+		if(event.which === 39 || event.which === 68){
+			counter = (counter+1)%5;
+		}
+		console.log(counter)
+	});
+	*/
+	
+	$(".owl-carousel").owlCarousel({
+		nav:true,
+		loop:true,
+		items:1
+		
+	});
+})
